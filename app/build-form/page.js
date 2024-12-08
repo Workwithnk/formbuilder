e188e28a108cc39326521813f30152ab4062e207
nikhilkavathiya <nikhilkavathiya.com>
2024-12-08 17:04:21 +0530

    Main: Added suspense on compo

--- app/build-form/page.js
+++ app/build-form/page.js
@@ -1,16 +1,15 @@
 'use client';
-import { useEffect, useState } from "react";
 import AddQuestion from "@/components/form/AddQuestion";
 import FormHeader from "@/components/form/FormHeader"
 import DragDropList from "@/components/form/DragDropList";
 import FormFooter from "@/components/form/FormFooter";
 import { useSelector } from "react-redux";
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from "react";
 
 
-function BuildForm() {
+function BuildFormInner() {
   const formData = useSelector(state => state.formReducer)
-  console.log("FORM DATA >>>", formData);
   const searchParams = useSearchParams();
   const formKey = searchParams.get('formKey')
   const thisForm = formData.length > 0 && formData.filter(data => data.key === formKey)[0]
@@ -20,8 +19,15 @@ function BuildForm() {
       {formData.length > 0 && <DragDropList />}
       <AddQuestion form={thisForm} />
       <FormFooter />
-    </div>
-  )
+    </div>)
 }
 
+export function BuildForm() {
+  return (
+    <Suspense>
+      <BuildFormInner />
+    </Suspense>
+  )
+}
 export default BuildForm
+
